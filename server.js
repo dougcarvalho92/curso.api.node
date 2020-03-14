0455be808c60e375ce76ff7f1ac112a0a6d86655
Douglas Carvalho <dscarvalho199@gmail.com>
2020-03-14 13:41:00 -0300

    Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+//INICIANDO APP
+const app = express();
+app.use(express.json()); // PERMITIR QUE ENVIA DADOS EM JSON PARA APLICAÇÃO
+app.use(cors());
+//INICIANDO O DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true ,useUnifiedTopology: true});
+
+requireDir('./src/models');
+
+//ROTAS
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
